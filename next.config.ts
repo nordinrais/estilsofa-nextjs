27c030951cfcdcd36bd19e1d25c383e667c98d74
nordinrais <nordinrais@gmail.com>
2026-01-24 17:14:02 +0100

    feat: add legacy redirect for oferta page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/pages/oferta.html',
+        destination: '/oferta',
+        permanent: true,
+      },
+      {
+        source: '/oferta.html',
+        destination: '/oferta',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
