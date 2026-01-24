796757eb22b09653c3b80bbe73794ca6fb360fbe
nordinrais <nordinrais@gmail.com>
2026-01-24 18:00:59 +0100

    Add redirects for all legacy HTML URLs to new Next.js routes

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   async redirects() {
     return [
+      // Oferta page
       {
         source: '/pages/oferta.html',
         destination: '/oferta',
@@ -12,6 +13,61 @@ const nextConfig: NextConfig = {
       {
         source: '/oferta.html',
         destination: '/oferta',
+        permanent: true,
+      },
+      // Blog page
+      {
+        source: '/pages/blog.html',
+        destination: '/blog',
+        permanent: true,
+      },
+      {
+        source: '/blog.html',
+        destination: '/blog',
+        permanent: true,
+      },
+      // Nuestros sofás page
+      {
+        source: '/pages/nuestros-sofas.html',
+        destination: '/nuestros-sofas',
+        permanent: true,
+      },
+      {
+        source: '/nuestros-sofas.html',
+        destination: '/nuestros-sofas',
+        permanent: true,
+      },
+      // Sobre nosotros page
+      {
+        source: '/pages/sobre-nosotros.html',
+        destination: '/sobre-nosotros',
+        permanent: true,
+      },
+      {
+        source: '/sobre-nosotros.html',
+        destination: '/sobre-nosotros',
+        permanent: true,
+      },
+      // Visítanos page
+      {
+        source: '/pages/visitanos.html',
+        destination: '/visitanos',
+        permanent: true,
+      },
+      {
+        source: '/visitanos.html',
+        destination: '/visitanos',
+        permanent: true,
+      },
+      // Index redirects
+      {
+        source: '/index.html',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/pages/index.html',
+        destination: '/',
         permanent: true,
       },
     ]
